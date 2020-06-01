02d021cdfe39e35a563beb62aa56782ffaef6de0
Ewen Tonnerre <ewen.tonnerre@etudiant.univ-lr.fr>
2020-06-01 22:13:37 +0200

    class Player

---
+++ PTS2_C2_1/RF_V1/src/rf_v1/Player.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rf_v1;
+
+import java.util.ArrayList;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author ewen
+ */
+public class Player {
+    private final String name;
+    private double posX;
+    private double posY;
+    private int speed;
+    private ImageView skin;
+    private int damage = 1;
+
+    public Player(ImageView aSkin, double aPosX,double aPosY) {
+        this.name = "jammy";
+        posX = aPosX;
+        posY=aPosY;
+        skin = aSkin;
+    }
+    
+    public double getX(){
+        return posX;
+    }
+    
+    public double getY(){
+        return posY;
+    }
+    
+    public int getSpeed(){
+        return speed;
+    }
+    
+    public void setSpeed(int aSpeed){
+        speed = aSpeed;
+    }
+    
+    public void setSkin(ImageView aSkin){
+        skin=aSkin;
+        skin.setX(posX);
+    }
+    
+    public void calculatePosition(){
+        posX += speed;
+    }
+    
+    public void draw(){
+        calculatePosition();
+        skin.setX(posX);
+    }
+}
+
