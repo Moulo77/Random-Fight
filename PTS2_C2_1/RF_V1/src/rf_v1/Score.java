f94b5e1aeeab0d7b56cbd618a630b4c577986a77
Leo Portier <leo.portier@etudiant.univ-lr.fr>
2020-06-02 11:05:37 +0200

    Upload New File

---
+++ PTS2_C2_1/RF_V1/src/rf_v1/Score.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rf_v1;
+
+/**
+ *
+ * @author mypc
+ */
+public class Score {
+    private int points;
+    
+    public Score(){
+        points = 0;
+    }
+    
+    public int getScore(){
+        return points;
+    }
+    
+    public void addPoints(int points){
+        this.points+=points;
+    }
+    
+    public void resetPoints(){
+        points = 0;
+    }
+}
