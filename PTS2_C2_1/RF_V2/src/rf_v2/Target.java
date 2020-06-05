afab4d70ac3e52f30dc29d3e7897d6fb55f143e0
Leo Portier <leo.portier@etudiant.univ-lr.fr>
2020-06-05 09:27:58 +0200

    Upload New File

---
+++ PTS2_C2_1/RF_V2/src/rf_v2/Target.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rf_v2;
+
+import javafx.scene.Parent;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author Palès
+ */
+public class Target extends Parent{
+    private int points;
+    private int lifePoints;
+    private double posX;
+    private double posY;
+    private ImageView skin;
+
+    public Target(ImageView skin, int points, int lifePoints, double posX, double posY) {
+        this.points = points;
+        this.lifePoints = lifePoints;
+        this.posX = posX;
+        this.posY = posY;
+        this.skin = skin;
+    }
+    
+    public double getX(){
+        return posX;
+    }
+    
+    public void setX(double aX){
+        posX=aX;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+    
+    public void setLife(int aLife){
+        lifePoints=aLife;
+    }
+    
+    public void setPoints(int points){
+        this.points=points;
+    }
+
+    public int getLifePoints() {
+        return lifePoints;
+    }
+    
+    public void hit(int damage){
+        lifePoints-=damage;
+    }
+    
+    public boolean isAlive(){
+        boolean alive = true;
+        if(lifePoints<=0){
+            alive=false;
+        }
+        
+        return alive;
+    }
+
+    public double getPosX() {
+        return posX;
+    }
+    
+    public void draw(){
+        skin.setX(posX);
+        skin.setY(posY);
+    }
+    
+}
