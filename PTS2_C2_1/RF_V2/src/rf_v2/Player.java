f82e586392adb0b85e7dfa5baa7c449af11e266b
Leo Portier <leo.portier@etudiant.univ-lr.fr>
2020-06-05 09:27:40 +0200

    Upload New File

---
+++ PTS2_C2_1/RF_V2/src/rf_v2/Player.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rf_v2;
+
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author ewen
+ */
+public class Player {
+    private final String name;
+    private double posX;
+    private double posY;
+    private int speed;
+    private ImageView skin;
+    private int damage = 1;
+
+    public Player(ImageView aSkin, double aPosX,double aPosY) {
+        this.name = "jammy";
+        posX = aPosX;
+        posY=aPosY;
+        skin = aSkin;
+        speed=0;
+        draw();
+    }
+    
+    public double getX(){
+        return posX;
+    }
+    
+    public double getY(){
+        return posY;
+    }
+    
+    public int getSpeed(){
+        return speed;
+    }
+    
+    public void setX(double aX){
+        posX=aX;
+    }
+    
+    public int getDamage(){
+        return damage;
+    }
+    
+    public void setSpeed(int aSpeed){
+        speed = aSpeed;
+    }
+    
+    public void setSkin(ImageView aSkin){
+        skin=aSkin;
+        skin.setX(posX);
+    }
+    
+    public ImageView getSkin(){
+        return skin;
+    }
+    
+    public void calculatePosition(){
+        posX += speed;
+        if(posX >= 730){
+            posX=730;
+        } else if(posX<=0){
+            posX=0;
+        }
+    }
+    
+    public void draw(){
+        calculatePosition();
+        skin.setX(posX);
+        skin.setY(posY);
+    }
+}
+
