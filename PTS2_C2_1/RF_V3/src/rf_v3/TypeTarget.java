6625c4eb981f8350656b3606cf9c57b2d5123b61
Ewen Tonnerre <ewen.tonnerre@etudiant.univ-lr.fr>
2020-06-18 09:28:35 +0200

    Upload New File

---
+++ PTS2_C2_1/RF_V3/src/rf_v3/TypeTarget.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rf_v3;
+
+/**
+ *
+ * @author ewen
+ */
+public enum TypeTarget {
+    SIMPLE,
+    MEDIUM,
+    HARD,
+    FAST,
+    BOSS
+}
